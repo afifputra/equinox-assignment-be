ec88e420b012ab88407c0ae5e5472442658466f1
Afif Putra H <afif@softwareseni.com>
2024-03-14 14:48:10 +0700

    fix: add types helmet and cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
-const port = 6969;
+const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
 
 app.get("/", (_: Request, res: Response) => {
   res.json({ message: "This is me" });
